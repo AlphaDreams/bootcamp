24ed4dcbb3d5d4c6a284fb7b4c3f7d97257c8bcc
alpha.kong <alphakth@gmail.com>
2025-04-08 12:57:38 +0800

    update

--- Helloworld.java
+++ Helloworld.java
@@ -6,10 +6,12 @@ public class Helloworld {
     //3 is an integer value
     //x is an integer variable.
     //You are putting 3 int the variable
-    int  x = -15;
+    int  x = 5;
+    int  y = 3;
+    int  z = x+y;
 
     //Print out
-    System.out.println(x);
+    System.out.println(z);
 
     System.out.println("welcome to bootcamp!!!");
   }
---
+++ demp-data-type/src/DemoDouble.java
@@ -0,0 +1,44 @@
+public class DemoDouble {
+  public static void main(String[] agrs) {
+    int x = 9;
+
+    //Store a number with decimal places
+    // Type: double
+    double y = 9.9;
+    System.out.println(y); //9.9
+
+    //! We cannot store d.p. into integer variable
+    //x = 9.9
+
+    //Store 99.9 into variable price
+    //Store 3 into variable quqntity
+    //calculate checkout amount
+
+    double price = 99.9;
+    int quantity = 3;
+    double amount = price * quantity;
+    System.out.println(amount); //299.70000000000005
+
+    //! Problem for "double"
+    double o1 = 0.1 + 0.2;
+    System.out.println(o1); // 0.300000000004
+
+    //! int / int > int
+    //10 > int value
+    //3 > int value
+    //1. 10 / 3 > int / int > int  (10 / 3 > 3)
+    //2. assign result into w (3 > 3.0)
+
+    //! double / int > double
+    int score1 = 71;
+    int score2 = 82;
+    //calculate teh average score
+    // Step1: (int + int) / int > int (71+82) / 2 > 76
+    // Step2: 76 > 76.0
+    //calculate the average score
+    double averageScore = (score1+score2) /2;
+    System.out.println(averageScore); //76.0
+
+
+  }
+}
